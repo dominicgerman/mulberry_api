74b9be4ddf845e74d42965b31d808083c89ffd7e
Dominic German <dominicgerman@gmail.com>
2024-07-06 19:57:19 -0500

    Added vendoring and basic server

--- main.go
+++ main.go
@@ -1,14 +1,28 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi"
 	"github.com/joho/godotenv"
 )
 
 func main() {
 	godotenv.Load()
 	port := os.Getenv("PORT")
-	fmt.Printf("Hey there %s\n", port)
+
+	router := chi.NewRouter()
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + port,
+	}
+
+	log.Printf("Serving on port %s", port)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
