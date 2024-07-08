4df441d484ab72791ae9dfd3a7c530b0b95569e8
Dominic German <dominicgerman@gmail.com>
2024-07-07 21:59:56 -0500

    Create user endpoint

--- main.go
+++ main.go
@@ -1,19 +1,52 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/dominicgerman/mulberry_api/internal/database"
 	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
+
+type apiConfig struct {
+	DB *database.Queries
+}
 
 func main() {
 	godotenv.Load()
 	port := os.Getenv("PORT")
+	dbUrl := os.Getenv("DB_URL")
+
+	db, err := sql.Open("postgres", dbUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	apiCfg := apiConfig{
+		DB: database.New(db),
+	}
+
+	corsOptions := cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		AllowCredentials: true,
+		MaxAge:           300, // 5 minutes
+	}
 
 	router := chi.NewRouter()
+	router.Use(cors.Handler(corsOptions))
+
+	v1Router := chi.NewRouter()
+	v1Router.HandleFunc("/healtz", handlerReadiness)
+	v1Router.Post("/users", apiCfg.handlerUsersCreate)
+
+	router.Mount("/v1", v1Router)
 
 	srv := &http.Server{
 		Handler: router,
@@ -21,7 +54,7 @@ func main() {
 	}
 
 	log.Printf("Serving on port %s", port)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
